e435ba6681e9c358acd5681c227052ea1dc92135
ZephiroRyde <104146932+ZephiroRyde@users.noreply.github.com>
2022-05-05 23:25:29 -0300

    empezando rotacion

---
+++ Assets/Scripts/Piero/Interactuables/ValveRotation.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValveRotation : MonoBehaviour
+{
+
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private Transform platform;
+    [SerializeField] private float _maxRot, _minRot;
+    [SerializeField] private Material[] _materials;
+    [SerializeField] private MeshRenderer _selfMesh;
+    [SerializeField] private float _detectDistance;
+
+    private void Update()
+    {
+        if (GameManager.GetInstance.GetPlayerController._interacting)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (platform.rotation.x >= _minRot)
+                {
+                    platform.eulerAngles += new Vector3(-_rotateSpeed * Time.deltaTime, 0f, 0f);
+                }
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                if(platform.rotation.x <= _maxRot)
+                {
+                    platform.eulerAngles += new Vector3(_rotateSpeed * Time.deltaTime , 0f, 0f);
+                }
+                
+            }
+        }
+
+        if (Vector3.Distance(transform.position, GameManager.GetInstance.GetPlayerController.ptransform) < _detectDistance)
+        {
+            _selfMesh.material = _materials[1];
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.GetInstance.GetPlayerController.Interact();
+            }
+        }
+        else
+        {
+            _selfMesh.material = _materials[0];
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, _detectDistance);
+    }
+}
