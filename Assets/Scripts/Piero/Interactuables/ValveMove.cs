44e1278e96018ecd14db45c70cb8958e3aed9acf
Fanca123 <bfaincaig@gmail.com>
2022-05-10 16:44:30 -0300

    Palancas Brillan
    Cutre

--- Assets/Scripts/Piero/Interactuables/ValveMove.cs
+++ Assets/Scripts/Piero/Interactuables/ValveMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ValveMove : MonoBehaviour
 {
+    
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform platform;
     [SerializeField] private Transform[] locations;
